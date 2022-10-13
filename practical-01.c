54df909e3e53ad7856892e63c97aba4a411a6801
Samir Jadhav <samirzjadhav@gmail.com>
2022-10-13 16:00:48 +0530

    misc: first practical

---
+++ practical-01.c
@@ -0,0 +1,21 @@
+// To find the area and circumference of a circle
+
+#define PI 3.14
+#include <stdio.h>
+
+int main(){
+    float radius,area,circum;
+    printf("\nEnter the radius of Circle :");
+    scanf("%f",&radius);
+
+    area =PI*radius*radius;
+    circum=2*PI*radius;
+    printf("\nArea of circle: %f",area);
+    printf("\n circumference =%f\n",circum);
+   return 0;
+}
+
+// Output:
+// Enter the radius of Circle : 2
+// Area of Circle : 12.560000
+// circumference = 12.560000
