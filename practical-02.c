737804ccd07cce0cc27d7d8f4427d75f89a303a2
Samir Jadhav <samirzjadhav@gmail.com>
2022-10-13 18:32:25 +0530

    misc: practical-02

---
+++ practical-02.c
@@ -0,0 +1,32 @@
+// C program to enter marks of five subjects and find percentage and grade
+#include <stdio.h>
+ 
+ int main(){
+    int phy,chem,bio,math,comp; // Five subject
+    float per;
+
+    // read marks of five subject from user
+    printf("Enter five subjects marks:");
+    scanf("%d%d%d%d%d",&phy,&chem,&bio,&math,&comp);
+
+    // Calculates Percentage
+    per=(phy+chem+bio+math+comp)/5.0;
+    printf("Percentage=%2f/n",per);
+
+    // finds grade ccording to the percentage 
+    if(per>90)printf("Grade A");
+    else if(per>80)printf("Grade B");
+    else if(per>70)printf("Grade C");
+    else if(per>60)printf("Grade D");
+    else if(per>40)printf("Grade E");
+    else printf("Grade F");
+ }
+
+// Output:
+// Enter five subjects marks: 55
+// 6612
+// 77
+// 88
+// 99
+// Percentage = 77.00
+// Grade C
