c9a1fc3b35a6158053f09bf1eb26604864202458
Samir Jadhav <samirzjadhav@gmail.com>
2022-11-07 23:13:10 +0530

    misc: practical-15

---
+++ practical-15.c
@@ -0,0 +1,66 @@
+// 15. To count vowels, consonants and white spaces Algorithm to count vowels,
+//     consonants and white spaces
+//     Program to count vowels,consonants and white spaces
+
+#include <ctype.h>
+#include <stdio.h>
+int main() {
+
+  char line[150];
+  int vowels, consonant, digit, space;
+
+  // initialize all variables to 0
+  vowels = consonant = digit = space = 0;
+
+  // get full line of string input
+  printf("Enter a line of string: ");
+  fgets(line, sizeof(line), stdin);
+
+  // loop through each character of the string
+  for (int i = 0; line[i] != '\0'; ++i) {
+
+    // convert character to lowercase
+    line[i] = tolower(line[i]);
+
+    // check if the character is a vowel
+    if (line[i] == 'a' || line[i] == 'e' || line[i] == 'i' || line[i] == 'o' ||
+        line[i] == 'u') {
+
+      // increment value of vowels by 1
+      ++vowels;
+    }
+
+    // if it is not a vowel and if it is an alphabet, it is a consonant
+    else if ((line[i] >= 'a' && line[i] <= 'z')) {
+      ++consonant;
+    }
+
+    // check if the character is a digit
+    else if (line[i] >= '0' && line[i] <= '9') {
+      ++digit;
+    }
+
+    // check if the character is an empty space
+    else if (line[i] == ' ') {
+      ++space;
+    }
+  }
+
+  printf("Vowels: %d", vowels);
+  printf("\nConsonants: %d", consonant);
+  printf("\nDigits: %d", digit);
+  printf("\nWhite spaces: %d", space);
+
+  return 0;
+}
+
+// Output:
+// Enter a line of string: abcde
+// Vowels: 2
+// Consonants: 3
+// Digits: 0
+// White spaces: 0Enter a line of string: santaji mahavidyalaya
+// Vowels: 9
+// Consonants: 11
+// Digits: 0
+// White spaces: 1
